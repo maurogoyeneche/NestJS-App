1cafe44207c6448e4af70bdfa7b7642e75cd26bd
Gino Bocacorsicopicolino <gnoboca@gmail.com>
2023-11-03 21:47:14 -0300

    route prefx api added

--- src/main.ts
+++ src/main.ts
@@ -3,8 +3,9 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.setGlobalPrefix('api');
   await app.listen(3000, () => {
-    console.log('Server listen in port 3000');
+    console.log(`Server listen in port ${process.env.PORT}`);
   });
 }
 bootstrap();
